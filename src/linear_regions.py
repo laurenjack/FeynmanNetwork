be2c04a05f02eb77085bd227e36b306f75ba1706
Jack Laurenson <jack@Jacks-Air.lan>
2017-05-31 16:57:15 +1200

    Initial work building region tree completed

--- src/linear_regions.py
+++ src/linear_regions.py
@@ -3,18 +3,69 @@ import numpy as np
 class RegionSetBuilder:
 
     def __init__(self, conf):
-        #Create map to store regions by layer
         self.sizes = conf.sizes
-        num_layers = len(self.sizes) - 2
-        self.layer_map = {}
-        for l in xrange(1, num_layers+1):
-            #Create set to hold all t's (the tuple id) that occur at that layer
-            self.layer_map[l] = {}
+        self.num_layers = len(self.sizes) - 2
+        self.hash_powers = self._create_hash_powers(self.sizes)
+        self.roots = {}
 
-
-    def putRegion(self, T_by_layer):
+    def putRegion(self, T_by_layer, targets, predictions):
         #Re-organise ts according to instances rather than layers
         all_T = self._transpose(T_by_layer)
+
+        #Add each linear region to the Tree
+        for i in xrange(len(all_T)):
+            T = all_T[i]
+            target = targets[i]
+            predicted = predictions[i]
+            l = 0
+            is_present = True
+            parent = None
+            while l < self.num_layers and is_present:
+                t = T[l]
+                is_present, parent = self._is_present(t, l, parent)
+                l += 1
+            final_reg = self._fill_unique(l, T, parent)
+            self._add_prediction_to(final_reg, target, predicted)
+
+    def get_forest(self):
+        return RegionForest(self.roots)
+
+    def _is_present(self, t, l, parent):
+        """Check if the current region t at layer l is already present in the region tree. If
+        it is return true and the equivalent region. If it isn't return false and the newly
+        created Region"""
+        if parent is None:
+            children = self.roots
+        else:
+            children = parent.sub_regions
+        key = self._hash_t(t, l)
+        if key in children.keys():
+            return True, children[key]
+        return False, self._new_region(l, t, parent, key)
+
+    def _fill_unique(self, l0, T, parent):
+        for l in xrange(l0, self.num_layers):
+            t = T[l]
+            key = self._hash_t(t, l)
+            parent = self._new_region(l, t, parent, key)
+        return parent
+
+    def _new_region(self, l, t, parent, key):
+        #Create the new region
+        if l == self.num_layers - 1:
+            new_region = FinalRegion(l, t, parent)
+        else:
+            new_region = Region(l, t, parent)
+        #Add it to its parent
+        if parent is None:
+            self.roots[key] = new_region
+        else:
+            parent.sub_regions[key] = new_region
+        return new_region
+
+    def _add_prediction_to(self, final_reg, target, predicted):
+        predictions = final_reg.predictions
+        predictions.append(Prediction(target, predicted))
 
     def _transpose(self, T_by_layer):
         """Transpose the list T_by_layer to have a top level ordering based on each
@@ -32,44 +83,96 @@ class RegionSetBuilder:
             T = []
             #Then over n layers
             for l in xrange(n):
-                t = T_by_layer[b][l]
+                t = T_by_layer[l][b]
                 T.append(t)
             all_T.append(T)
         return all_T
 
-    # def _create_hash_powers(self, sizes):
-    #     """Create the powers that each index i of t is raised to, so that each t may be represented
-    #     by a unique integer"""
-    #     hash_powers = []
-    #     for l in sizes[1:-1]:
-    #         hp = np.zeros(shape=l, dtype=np.long)
-    #         for i in xrange(l):
-    #             np.zeros[i] = 2 << i
-    #         hash_powers.append(hp)
-    #     return hash_powers
+    def _create_hash_powers(self, sizes):
+        """Create the powers that each index i of t is raised to, so that each t may be represented
+        by a unique integer"""
+        hash_powers = []
+        for l in sizes[1:-1]:
+            hp = np.zeros(shape=l, dtype=np.long)
+            for i in xrange(l):
+                hp[i] = 1 << (l - i - 1)
+            hash_powers.append(hp)
+        return hash_powers
+
+    def _hash_t(self, t, l):
+        return np.sum(self.hash_powers[l] * t)
+
+class RegionForest:
+    """Data structure representing all the active regions in a network for
+    a given test set"""
+
+    def __init__(self, roots):
+        self.roots = roots
+
+    def all_final_ts(self):
+        ts = {}
+        self._all_final_ts(self.roots, ts)
+        return ts
 
 
+    def get_frequency_each_region(self, at_layer):
+        num_pred_map = {}
+        self._get_frequency_each_region(self.roots, num_pred_map, 1, at_layer)
+        num_pred_map[1] = FreqTracker()
+        return num_pred_map
+
+    def _all_final_ts(self, regions, ts):
+        for key, T in regions.iteritems():
+            if isinstance(T, FinalRegion):
+                if key in ts:
+                    ts[key] += 1
+                else:
+                    ts[key] = 1
+            else:
+                self._all_final_ts(T.sub_regions, ts)
+
+
+    def _get_frequency_each_region(self, regions, num_pred_map, l, at_layer):
+        for _, T in regions.iteritems():
+            if l == at_layer:
+                self._update_freq(T, num_pred_map)
+            else:
+                self._get_frequency_each_region(T.sub_regions, num_pred_map, l+1, at_layer)
+
+
+    def _update_freq(self, T, num_pred_map):
+        cor, inc = T.num_predictions()
+        num_pred = cor + inc
+        is_pure = T.is_pure()
+        if num_pred in num_pred_map.keys():
+            freq_tracker = num_pred_map[num_pred]
+        else:
+            freq_tracker = FreqTracker()
+            num_pred_map[num_pred] = freq_tracker
+        if is_pure:
+            freq_tracker.freq_perfect += 1
+        else:
+            freq_tracker.freq_mixed += 1
 
 
 
 
 class Region:
     """Data structure representing a linear region at a given layer"""
-    def __init__(self):
-        self.instances = {}
-        self.layer = None
-        self.t = None
-        self.sub_regions = []
-        self.super_region = None
+    def __init__(self, l, t, super_region):
+        self.l = l
+        self.t = t
+        self.super_region = super_region
+        self.sub_regions = {}
 
     def __eq__(self, other):
-        if not isinstance(other, Region):
+        if not isinstance(other, self.__class__):
             return False
         # if self.layer is None or self.id is None:
         #     raise TypeError("")
-        if self.layer != other.layer:
+        if self.l != other.l:
             return False
-        if self. t!= other.t:
+        if not np.array_equal(self. t, other.t):
             return False
         if self.super_region != other.super_region:
             return False
@@ -78,14 +181,68 @@ class Region:
     def __ne__(self, other):
         return not self.__eq__(self, other)
 
+    def num_predictions(self):
+        tot_corr, tot_inc = 0, 0
+        for _, T in self.sub_regions.iteritems():
+            corr, inc = T.num_predictions()
+            tot_corr += corr
+            tot_inc += inc
+        return tot_corr, tot_inc
+
+    def get_predictions(self):
+        preds = []
+        for T in self.sub_regions:
+            preds.extend(T.get_predictions)
+        return preds
+
+    def is_pure(self):
+        first = True
+        targ = None
+        for _, T in self.sub_regions.iteritems():
+            is_pure, target = T.is_pure()
+            if not is_pure:
+                return False
+            if first:
+                first = False
+                targ = target
+            elif targ != target:
+                return False
+        return True, target
 
 
+class FinalRegion(Region):
 
+    def __init__(self, l, t, super_region):
+        Region.__init__(self, l, t, super_region)
+        self.predictions = []
 
+    def num_predictions(self):
+        correct = sum(int(p.target == p.predicted) for p in self.predictions)
+        incorrect = len(self.predictions) - correct
+        return correct, incorrect
 
+    def get_predictions(self):
+        return self.predictions
 
-class Instance:
+    def is_pure(self):
+        target = self.predictions[0].target
+        for pred in self.predictions:
+            if pred.target != target:
+                return False
+        return True, target
+
+class Prediction:
+
+    def __init__(self, target, predicted):
+        self.target = target
+        self.predicted = predicted
+
+    def is_correct(self):
+        return self.target == self.predicted
+
+class FreqTracker:
 
     def __init__(self):
-        self.correct = 0
-        self.incorrect = 0
+        self.freq_perfect = 0
+        self.freq_mixed = 0
+
--- src/network.py
+++ src/network.py
@@ -6,53 +6,65 @@ class FeedForward:
     active linear regions and their properties"""
 
     def __init__(self, conf):
-
         self.sizes = conf.sizes
+        self.learning_rate = conf.learning_rate
+        self.num_h_layers = len(self.sizes) - 2
+        in_dim = self.sizes[0]
+        out_dim = self.sizes[-1]
+        # Create place holder for inputs and target outputs
+        self.x = tf.placeholder(tf.float32, shape=[None, in_dim], name="inputs")
+        self.y = tf.placeholder(tf.float32, shape=[None, out_dim], name="target_outputs")
 
-    def train(self):
-        pass
+        #Create the network
+        self.T = []
+        a_out = self.x
+        for s_in, s_out, l in zip(self.sizes[:-2], self.sizes[1:-1], xrange(self.num_h_layers)):
+            a_out = self._create_layer(s_in, s_out, a_out, l)
+            self.T.append(tf.greater(a_out, 0))
 
-    def cost(self):
-        pass
+        # Create softmax layer for classification
+        l = len(self.sizes) - 2
+        self.a_out = self._create_layer(self.sizes[-2], self.sizes[-1], a_out, l, act_func=tf.nn.softmax)
 
     def feed_forward(self):
-        #Create place holder for inputs
-        in_dim = self.sizes[0]
-        x = tf.placeholder(tf.float32, shape=[None, in_dim], name="z_inputs")
+        return self.a_out, self.T
 
-        #Create hidden layers
-        T = []
-        a_out = x
-        for s_in, s_out in zip(self.sizes[:-2],self.sizes[1:-1]):
-            a_out = self._hidden_layer(s_in, s_out, a_out)
-            T.append(tf.greater(a_out, 0), name="make_t")
+    def train(self):
+        a, _ = self.feed_forward()
+        #TODO train for zero too
+        cross_entropy = tf.reduce_mean(-tf.reduce_sum(self.y * tf.log(a), reduction_indices=[1]))
+        train_op = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(cross_entropy)
+        return train_op
 
-        #Create softmax layer for classification
-        a_out = self._hidden_layer(s_in, s_out, a_out, act_func=tf.nn.softmax)
-        return a_out, T
+    def accuracy(self):
+        a, _ = self.feed_forward()
+        is_correct = tf.equal(tf.argmax(self.y, 1), tf.argmax(a, 1))
+        return tf.reduce_mean(tf.cast(is_correct, tf.float32))
 
-    def _create_layer(self, s_in, s_out, a_in, act_func=tf.nn.relu):
+    def get_inaccurate_indicies(self):
+        a, T = self.feed_forward()
+        predictions = tf.argmax(a, axis=1)
+        actuals = tf.argmax(self.y, axis=1)
+
+
+    def make_prediction(self):
+        a, T = self.feed_forward()
+        predictions = tf.argmax(a, axis=1)
+        return predictions, T
+
+    def _create_layer(self, s_in, s_out, a_in, l, act_func=tf.nn.relu):
         #name the parameters
-        w_name = "W" + str(s_out)
-        b_name = "b" + str(s_out)
+        w_name = "W" + str(l)
+        b_name = "b" + str(l)
 
         #Create parameters
         W = tf.get_variable(w_name, shape=[s_in, s_out], initializer=tf.contrib.layers.xavier_initializer())
-        b = tf.Variable(0.1, dtype=tf.float32, shape=[s_out], name=b_name)
+        b = tf.Variable(0.1 * np.ones(s_out), dtype=tf.float32, name=b_name)
 
         #Apply the weighted sum and activation
         z = tf.add(tf.matmul(a_in, W), b)
         a_out = act_func(z)
         return a_out
-
-    def _softmax_layer(self, s_in, s_out, a_in):
-        W, b = self._create_weights(s_in, s_out)
-        z = tf.add(tf.matmul(a_in, W), b)
-        return t
-
-    def _create_weights(self, s_in, s_out):
-
-        return W, b
 
 
 
